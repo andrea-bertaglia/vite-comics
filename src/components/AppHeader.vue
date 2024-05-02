32facdafebef718c231bcadf02fe611117792da4
andrea-bertaglia <andreabertaglia@gmail.com>
2024-05-02 17:45:08 +0200

    feat: header component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,32 +2,107 @@
 export default {
   data() {
     return {
-
+      menu: [
+        {
+          title: "Characters",
+          isActive: false,
+        },
+        {
+          title: "Comics",
+          isActive: true,
+        },
+        {
+          title: "Movies",
+          isActive: false,
+        },
+        {
+          title: "Tv",
+          isActive: false,
+        },
+        {
+          title: "Games",
+          isActive: false,
+        },
+        {
+          title: "Collectibles",
+          isActive: false,
+        },
+        {
+          title: "Videos",
+          isActive: false,
+        },
+        {
+          title: "Fans",
+          isActive: false,
+        },
+        {
+          title: "News",
+          isActive: false,
+        },
+        {
+          title: "Shop",
+          isActive: false,
+        }
+      ],
     }
   }
 }
 </script>
 
 <template>
-    <header>
-        <div class="container">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, tenetur nobis cupiditate, blanditiis nisi alias fuga, quas nesciunt ut tempore omnis sit maiores? Veniam, ipsa id ipsam temporibus earum excepturi.</p>
-        </div>
-    </header>
+  <header>
+    <div class="container">
+      <div class="header-logo">
+        <img src="../assets/img/dc-logo.png" alt="Logo DC Comics">
+      </div>
+
+      <ul class="header-menu">
+        <li v-for="item in menu">
+          <a href="#" :class="{ active: item.isActive }"> {{ item.title.toUpperCase() }}</a>
+        </li>
+      </ul>
+
+    </div>
+  </header>
 </template>
 
 <style scoped lang="scss">
-
 @use "../style/partials/mixins" as *;
+@use "../style/partials/variables" as *;
 
 header {
-    @include flex(row, center, center);
-    
-    .container {
-        @include flex(row, center, center);
-        max-width: 80%;
-        background-color: lightcoral;
-}
-}
+  @include flex(row, center, center);
 
+  .container {
+    @include flex(row, space-between, center);
+    width: 80%;
+    max-width: 1440px;
+    height: 150px;
+
+    .header-menu {
+      @include flex(row, space-between, center);
+      list-style-type: none;
+      gap: 1em;
+
+      li {
+        height: 100%;
+        display: inline-block;
+
+        a {
+          text-decoration: none;
+          color: $black-color;
+
+          &:hover {
+            color: $primary-color;
+          }
+
+          &.active {
+            color: $primary-color;
+          }
+        }
+      }
+
+    }
+  }
+}
 </style>
